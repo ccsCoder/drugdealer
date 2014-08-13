32b3632d402f2661b15d62b501615089b31fda02
Nishant Gupta <ccsCoder@gmail.com>
2014-08-13 19:26:36 +0530

    -- Some Commit

---
+++ src/com/neo/drugdealer/MainDrugActivity.java
@@ -0,0 +1,39 @@
+package com.neo.drugdealer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainDrugActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_drug);
+		//Add button event.
+		final Button searchButton = (Button) findViewById(R.id.searchButton);
+		searchButton.setOnClickListener(new View.OnClickListener(	) {
+			
+			@Override
+			public void onClick(View v) {
+				
+			EditText textview= (EditText) findViewById(R.id.multiLineText);
+				//scrView.set
+			textview.setText("Clicked Man");
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_drug, menu);
+		return true;
+	}
+
+}
