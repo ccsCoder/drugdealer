8e8e9c91a2048b27b9c337c80e258860c3ca0d76
Nishant Gupta <ccsCoder@gmail.com>
2014-08-14 17:55:00 +0530

    -- List Adapters

---
+++ src/com/neo/drugdealer/CustomBaseExapandableListAdapter.java
@@ -0,0 +1,129 @@
+package com.neo.drugdealer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class CustomBaseExapandableListAdapter extends BaseExpandableListAdapter {
+	
+	private List<String> groupItems;
+	private List<String> childItems;
+	private List<Object> children = new ArrayList<Object>();
+	private LayoutInflater inflater;
+	private Activity activity;
+	
+	public CustomBaseExapandableListAdapter(List<String> groupList, List<Object> children) {
+		this.groupItems=groupList;
+		this.children =children;
+		
+	}
+	
+	public void setInflater(LayoutInflater inflater, Activity activity) {
+		this.inflater=inflater;
+		this.activity=activity;
+	}
+	
+		
+	@Override
+	public Object getChild(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getChildId(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView,ViewGroup parent) {
+		this.childItems = (List<String>) this.children.get(groupPosition);
+		TextView tv = null;
+		if(convertView == null) {
+			convertView = this.inflater.inflate(R.layout.childrow,null);
+		}
+		tv = (TextView) convertView.findViewById(R.id.textView1);
+		tv.setText(this.childItems.get(childPosition));
+		convertView.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(activity, childItems.get(childPosition), Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		
+		return convertView;
+	}
+
+	@Override
+	public int getChildrenCount(int groupPosition) {
+		// TODO Auto-generated method stub
+		return ((List<String>)this.children.get(groupPosition)).size();
+	}
+
+	@Override
+	public Object getGroup(int groupPosition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getGroupCount() {
+		// TODO Auto-generated method stub
+		return this.groupItems.size();
+	}
+
+	@Override
+	public long getGroupId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	 public void onGroupCollapsed(int groupPosition) {
+	  super.onGroupCollapsed(groupPosition);
+	 }
+
+	 @Override
+	 public void onGroupExpanded(int groupPosition) {
+	  super.onGroupExpanded(groupPosition);
+	 }
+
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		if(convertView==null) {
+			convertView= this.inflater.inflate(R.layout.grouprow,null);
+		}
+		((CheckedTextView) convertView).setText(this.groupItems.get(groupPosition));
+		((CheckedTextView) convertView).setChecked(isExpanded);	
+		return convertView;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isChildSelectable(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
---
+++ src/com/neo/drugdealer/MultipleItemsList.java
@@ -0,0 +1,118 @@
+package com.neo.drugdealer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ExpandableListActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+
+public class MultipleItemsList extends ExpandableListActivity implements OnChildClickListener{
+	
+	private List<String> groupItems = new ArrayList<String>();
+	private List<Object> childGroupItems = new ArrayList<Object>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_multiple_items_list);
+		// Show the Up button in the action bar.
+		setupActionBar();
+		ExpandableListView listView = getExpandableListView();
+		listView.setDividerHeight(2);
+		listView.setGroupIndicator(null);
+		listView.setClickable(true);
+		
+		//set parent group data
+		setGroupData();
+		//set child group data
+		setChildGroupData();
+		
+		//Create a new Adapter.
+		CustomBaseExapandableListAdapter customAdapter = new CustomBaseExapandableListAdapter(groupItems, childGroupItems);
+		
+		customAdapter.setInflater((LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
+		getExpandableListView().setAdapter(customAdapter);
+		listView.setOnChildClickListener(this);
+		
+	}
+
+	
+
+	private void setChildGroupData() {
+		// TODO Auto-generated method stub
+		this.childGroupItems.add("Treating symptoms of Headache, Nausea and Vertigo.");
+		this.childGroupItems.add("Calpol, Metacin");
+		this.childGroupItems.add("Liver Damage");
+		this.childGroupItems.add("5mg/day");
+		this.childGroupItems.add("--- Never eat with Oranges");
+		
+	}
+
+
+
+	private void setGroupData() {
+		// TODO Auto-generated method stub
+		this.groupItems.add("Used for");
+		this.groupItems.add("Brand/Components");
+		this.groupItems.add("Side Effects");
+		this.groupItems.add("Dosage");
+		this.groupItems.add("Additional");
+	}
+	
+	
+
+
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.multiple_items_list, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onChildClick(ExpandableListView arg0, View arg1, int arg2,
+			int arg3, long arg4) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
